eb9da180a0ad9f957cef392d561ebfbc5d23383c
Aayush Soni <aayushsoni1902@gmail.com>
2023-07-09 00:20:57 +0530

    Added Gallery section and made minor adjustments in other sections

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import "./Gallery.css"
 
 const Gallery = () => {
     return (
         <>
-            <div>Gallery</div>
+            <div id="gallery">
+                <h1>Our gallery</h1>
+
+                <div className="g-line"></div>
+
+                <div className="images">
+                    <img src="src/assets/g1.jpg" alt="Image 1" />
+                
+                    <img src="src/assets/g2.jpg" alt="Image 2" />
+                
+                    <img src="src/assets/g3.jpg" alt="Image 3" />
+                
+                    <img src="src/assets/g4.jpg" alt="Image 4" />
+                
+                    <img src="src/assets/g5.jpg" alt="Image 5" />
+
+                    <img src="src/assets/g6.jpg" alt="Image 6" />
+                </div>
+            </div>
         </>
     )
 }
