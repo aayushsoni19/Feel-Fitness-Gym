ea5955bb9b505843537ccba4b6b2d0b9a81750da
Aayush Soni <aayushsoni1902@gmail.com>
2023-07-09 17:48:50 +0530

    minor import update

--- src/container/LandingPage/LandingPage.jsx
+++ src/container/LandingPage/LandingPage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faYoutube } from '@fortawesome/free-brands-svg-icons'
-import Navbar from '../../components/Navbar/navbar'
+import Navbar from '../../components/Navbar/Navbar'
 library.add(faYoutube)
 import "./LandingPage.css"
 
