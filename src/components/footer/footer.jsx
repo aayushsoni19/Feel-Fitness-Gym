d3215861f4cccd52fe3910cffd2f64ba9f949c43
Aayush Soni <aayushsoni1902@gmail.com>
2023-07-09 17:37:51 +0530

    minor update

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -7,21 +7,21 @@ const Footer = () => {
         <div id="footer">
             <div className="footer-content">
                 <div className="logo">
-                    <img src="https://live.staticflickr.com/65535/53033914278_534a56baa0_n.jpg" alt="Logo" />
+                    <img src="src/assets/logo.png" alt="Logo" />
                     <div className="tagline">Transform Your Body, Transform Your Life</div>
                 </div>
                 <div className="footer-desc">                
                     <h2>Quick Contacts</h2>
                     <div className="address">
-                        <img src="https://live.staticflickr.com/65535/53033914348_8a14d7b9ab_s.jpg" alt="Location" />
+                        <img src="src/assets/address.png" alt="Location" />
                         <span>SF/TF RADHE COMPLEX, 52 RADHA KRISHNA PARK SOCIETY, AKOTA, VADODARA, GUJARAT, 390020</span>
                     </div>
                     <div className="phone-no">
-                        <img src="https://live.staticflickr.com/65535/53033434201_00c506c229_s.jpg" alt="Call us" />
+                        <img src="src/assets/phone.png" alt="Call us" />
                         <span>+91 9909912369</span>
                     </div>
                     <div className="mail">
-                        <img src="https://live.staticflickr.com/65535/53033914243_1753361726_s.jpg" alt="Mail" />
+                        <img src="src/assets/Mail.png" alt="Mail" />
                         <span>info@feelfitness.in</span>
                     </div>
                 </div>
--- src/container/LandingPage/LandingPage.jsx
+++ src/container/LandingPage/LandingPage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faYoutube } from '@fortawesome/free-brands-svg-icons'
-import Navbar from '../../components/navbar/navbar'
+import Navbar from '../../components/Navbar/navbar'
 library.add(faYoutube)
 import "./LandingPage.css"
 
