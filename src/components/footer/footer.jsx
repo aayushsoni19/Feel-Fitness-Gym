1028a7fe14694d6eb3a64ab4fba298a39a884e5c
Aayush Soni <aayushsoni1902@gmail.com>
2023-07-09 15:33:02 +0530

    added responsive footer code

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -4,27 +4,25 @@ import "./footer.css"
 const Footer = () => {
   return (
     <>
-        <div className="footer">
+        <div id="footer">
             <div className="footer-content">
                 <div className="logo">
-                    <img src="" alt="Logo" />
+                    <img src="src/assets/logo.png" alt="Logo" />
                     <div className="tagline">Transform Your Body, Transform Your Life</div>
                 </div>
-                <div className="footer-desc">
-                    <div className="qc">
-                        <h3>Quick Contacts</h3>
-                        <div className="address">
-                            <img src="" alt="Location" />
-                            <span>SF/TF RADHE COMPLEX, 52 RADHA KRISHNA PARK SOCIETY, AKOTA, VADODARA, GUJARAT, 390020</span>
-                        </div>
-                        <div className="phone-no">
-                            <img src="" alt="Call us" />
-                            <span>+91 9909912369</span>
-                        </div>
-                        <div className="mail">
-                            <img src="" alt="Mail" />
-                            <span>info@feelfitness.in</span>
-                        </div>
+                <div className="footer-desc">                
+                    <h2>Quick Contacts</h2>
+                    <div className="address">
+                        <img src="src/assets/address.png" alt="Location" />
+                        <span>SF/TF RADHE COMPLEX, 52 RADHA KRISHNA PARK SOCIETY, AKOTA, VADODARA, GUJARAT, 390020</span>
+                    </div>
+                    <div className="phone-no">
+                        <img src="src/assets/phone.png" alt="Call us" />
+                        <span>+91 9909912369</span>
+                    </div>
+                    <div className="mail">
+                        <img src="src/assets/Mail.png" alt="Mail" />
+                        <span>info@feelfitness.in</span>
                     </div>
                 </div>
             </div>
